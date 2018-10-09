ff263a908452238eeb30ec83eb2e2d104486edf7
irinamd31 <irina.md31@gmail.com>
2018-10-09 21:25:59 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+//Task1
+// var num = prompt();
+// console.log(num);
+// if (num>=25 && num <=99){
+//     console.log('number is in range');
+// } else {
+//     console.log('number is not in range');
+// }
+
+
+//Task2
+// var a = prompt(), b= prompt(), rezult;
+// console.log(a);
+// console.log(b);
+// rezult =  (a===b)? "a and b are equal" : a>b? "a is greater" : "b is greater";
+// console.log(rezult);
+
+
+//Task3
+//1. 10
+//2. -1
+//3. 1
+//4. 2
+//5. 6
+//6. 9px
+//7. $45
+//8. 2
+//9. NaN
+//10. infinity
+//11. -9
+//   5
+//12. -14
+//13. 2
+//14. 5
+//15. 5
+//16. 5
+//17. 1
+//18. NaN
+//19. false
+//20. true
